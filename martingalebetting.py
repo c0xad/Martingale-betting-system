bd8a55c3767b0cfbed44ebc6db3a4d4728aaf3ce
c0xad <69444504+c0xad@users.noreply.github.com>
2023-12-14 05:10:31 +0300

    Update martingalebetting.py

--- martingalebetting.py
+++ martingalebetting.py
@@ -1,33 +1,60 @@
-import requests
+import random
 
-class Martingale:
-  def __init__(self, initial_bet, goal, max_bet):
-    self.initial_bet = initial_bet
-    self.goal = goal
-    self.max_bet = max_bet
-    self.total_bet = 0
-    self.total_won = 0
-  
-  def bet(self):
-    bet = self.initial_bet
-    while True:
-      self.total_bet += bet
-      if self.total_bet > self.goal:
-        return -1, self.total_won
-      result = place_bet(bet)
-      if result:
-        self.total_won += bet
-        return bet, self.total_won
-      bet = min(bet*2, self.max_bet)
+class HybridBetting:
+    def __init__(self, initial_bankroll, goal, max_bet, stop_loss):
+        self.initial_bankroll = initial_bankroll
+        self.goal = goal
+        self.max_bet = max_bet
+        self.stop_loss_threshold = stop_loss
+        self.model = None  # Placeholder for your ML model
 
-def place_bet(bet):
-  # Send a request to the betting system API to place a bet
-  response = requests.post(
-    'https://example.com/api/place_bet',
-    json={'amount': bet}
-  )
-  # Check the response to see if the bet was successful
-  if response.status_code == 200:
-    return True
-  else:
-    return False
+    def train_model(self, data, target):
+        # Train your machine learning model on historical data and target variable (e.g., win/loss)
+        # This example uses a placeholder function, replace it with your actual model training logic
+        self.model = LogisticRegression()  # Replace with your chosen model
+        self.model.fit(data, target)
+
+    def backtest(self, data, simulations=1000):
+        results = []
+        for _ in range(simulations):
+            bankroll = self.initial_bankroll
+            consecutive_losses = 0
+            total_won = 0
+
+            for outcome in data:
+                predicted_win_probability = self.model.predict_proba(data)[0][1]  # Replace with your model's prediction method
+                # Calculate bet based on Kelly Criterion and predicted win probability
+                bet = self._calculate_bet(bankroll, predicted_win_probability)
+                bankroll -= bet
+                if random.random() < predicted_win_probability:
+                    bankroll += 2 * bet
+                    consecutive_losses = 0
+                else:
+                    consecutive_losses += 1
+                    if consecutive_losses >= self.stop_loss_threshold:
+                        break
+
+            results.append((bankroll, total_won))
+
+        return results
+
+    def _calculate_bet(self, bankroll, predicted_win_probability):
+        # Implement your logic for calculating the bet based on Kelly Criterion and predicted win probability
+        # This is a placeholder example, replace it with your actual strategy
+        edge = predicted_win_probability - (1 - predicted_win_probability)
+        kelly_fraction = edge / predicted_win_probability
+        optimal_bet = min(bankroll * kelly_fraction, self.max_bet)
+        bet = min(optimal_bet, 2 * self.consecutive_losses * self.initial_bet, bankroll, 1)
+        return bet
+
+# Example usage
+data = [features1, features2, ...]  # Replace with your actual data features
+target = [win/loss labels]  # Replace with labels for win/loss outcomes
+
+hybrid_betting = HybridBetting(1000, 2000, 500, 3)
+hybrid_betting.train_model(data, target)
+results = hybrid_betting.backtest(data, simulations=1000)
+
+# Analyze and visualize the results
+# ...
+
